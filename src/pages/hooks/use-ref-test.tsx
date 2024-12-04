a1e416ecca441c9fd7c22c5f7c7c9049bb29e8f6
fix-a-lot <fixalot@outlook.kr>
2024-12-04 12:13:51 +0900

    update: useRef() 테스트 코드 변경

--- src/pages/hooks/use-ref-test.tsx
+++ src/pages/hooks/use-ref-test.tsx
@@ -1,27 +1,50 @@
 import React, {useRef, useState, useEffect} from 'react';
 
 export default function UseRefTest(): React.JSX.Element {
-  const refTest = useRef(null);
-  const [value, setValue] = useState('');
+  const someRef = useRef(0);
+  const [someState, setSomeState] = useState(0);
 
-  useEffect(() => {
-    refTest.current = 123; // 첫 렌더링에 123 할당
-  }, []);
-
-  const onChange = event => setValue(event.target.value);
-
-  console.log(refTest.current); // 첫 렌더링엔 null, 이후 사용자 입력이 발생하면 123 출력됨
+  function log() {
+    console.log('someRef.current:', someRef.current);
+  }
 
   return (
     <section>
       <h2>useRef 테스트</h2>
-      <input
-        type="text"
-        autoFocus
-        value={value}
-        onChange={onChange}
-        placeholder="아무거나 입력하세요"
-      />
+      <button
+        style={{margin: 1}}
+        onClick={() => {
+          setSomeState(prev => prev + 1);
+        }}
+      >
+        state +1
+      </button>
+      <button
+        style={{margin: 1}}
+        onClick={() => {
+          someRef.current++;
+        }}
+      >
+        ref +1
+      </button>
+      <button style={{margin: 1}} onClick={log}>
+        console.log(ref)
+      </button>
+      <br />
+      <br />
+      someState: {someState}
+      <br />
+      someRef: {someRef.current}
+      <br />
+      <br />
+      <div className="important">
+        <p>state +1 버튼은 누를 때마다 someState 값이 증가하고 화면이 렌더링된다.</p>
+        <p>
+          ref +1 버튼은 누를 때마다 someRef 값이 증가하긴 하지만 화면 렌더링이 발생하진 않는다.
+          증가시킨 값은 someRef.current에 즉시 반영되는 걸 확인할 수 있다. (콘솔 출력 버튼 눌러볼
+          것)
+        </p>
+      </div>
     </section>
   );
 }
